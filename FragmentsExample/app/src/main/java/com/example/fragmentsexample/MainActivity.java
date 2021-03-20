05f73beb0c32d9a73b5bbfc6a03640e13fe94cb2
Rajeev <rajeev29five@gmail.com>
2021-03-21 02:37:54 +0530

    learning fragments in android

---
+++ FragmentsExample/app/src/main/java/com/example/fragmentsexample/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.fragmentsexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1, btn2;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        btn1 = findViewById(R.id.btnFragment1);
+        btn2 = findViewById(R.id.btnFragment2);
+        
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFirstFragment(new FirstFragment());
+            }
+        });
+        
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setSecondFragment(new SecondFragment());
+            }
+        });
+    }
+
+    private void setSecondFragment(Fragment secondFragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment, secondFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void setFirstFragment(Fragment firstFragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment, firstFragment);
+        fragmentTransaction.commit();
+    }
+}
